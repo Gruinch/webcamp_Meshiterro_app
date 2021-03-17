7e2e1a93601723b4172826872ce2805907ca4b52
Gruinch <sel.chemi86@gmail.com>
2021-03-17 13:37:34 +0000

    [Add] 一覧ページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c1d8dfd531ac84a549d10fdf6c272f7839411c195c8849f86fb2108f8aacfad0b61a631806471f3aa750a69a4b172c34aad91dc543bd1be65be96f807d45bb62'
