77da56653be88404b85661b77b78bddb3fdb883e
Gruinch <sel.chemi86@gmail.com>
2021-03-18 09:52:26 +0000

    [Add]show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
-  resources:post_images, only:[:new, :create, :index, :destroy]
+  resources:post_images, only:[:new, :create, :index,:show, :destroy]
 
 end
